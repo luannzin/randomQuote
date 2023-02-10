66a11132e9c72c75b0669126d868cef8ff3a6470
Luan Fabri <luandaniel966@gmail.com>
2023-02-10 11:57:45 -0300

    Delete index-eb58c6af.js

--- assets/index-eb58c6af.js
+++
@@ -1,7 +0,0 @@
-(function(){const o=document.createElement("link").relList;if(o&&o.supports&&o.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))c(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const a of t.addedNodes)a.tagName==="LINK"&&a.rel==="modulepreload"&&c(a)}).observe(document,{childList:!0,subtree:!0});function r(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerpolicy&&(t.referrerPolicy=e.referrerpolicy),e.crossorigin==="use-credentials"?t.credentials="include":e.crossorigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function c(e){if(e.ep)return;e.ep=!0;const t=r(e);fetch(e.href,t)}})();const s=async()=>{i("Carregando...");const o=await(await fetch("https://api.quotable.io/random")).json();l(o.content)},l=async n=>{const r=await(await fetch(`https://translate.googleapis.com/translate_a/single?client=gtx&sl=en&tl=pt&dt=t&q=${n}`)).json();r[0][0][0].length>80?s():i(r[0][0][0])},d=()=>{var n,o,r;(r=(o=(n=document.querySelector("#app"))==null?void 0:n.childNodes[1])==null?void 0:o.childNodes[5])==null||r.addEventListener("click",()=>{s()})},i=n=>{document.querySelector("#app").innerHTML=`
-    <div>
-        <h1>${n}</h1>
-        <p>Você pode gerar uma nova mensagem aqui!</p>
-        <button class="button">Nova Mensagem</button>
-    </div>
-`,d()};i("Carregando...");s();
